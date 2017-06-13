f74a02d19001a2fc295deb351de84134539e1f5b
Dillon Streator <dillonstreator@gmail.com>
2017-06-12 20:00:23 -0500

    added static pages: about, coffee_house, and coffee_club

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def index
   end
+  def about
+  end
+  def coffee_club
+  end
+  def coffee_house
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :employees
   devise_for :customers
   get "/" => "pages#index"
-  
+
+  get "/about" => "pages#about"
+  get "/coffee_club" => "pages#coffee_club"
+  get "/coffee_house" => "pages#coffee_house"
 end
