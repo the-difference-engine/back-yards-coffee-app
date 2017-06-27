dbea3ef7f0661c380771ffe6b3e6719b38b4adb7
James Hughes <james.a.hughes@gmail.com>
2017-06-26 20:33:47 -0500

    style devise login pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  layout :layout_by_resource
+
+  def layout_by_resource
+    if devise_controller? && !(current_employee || current_customer)
+      "devise"
+    else
+      "application"
+    end
+  end
 end
