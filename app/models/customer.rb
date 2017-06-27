e075e5d1b926624964d30f0db410e8c6b7d5d353
Rebecca Mutek <mutek.b@gmail.com>
2017-06-26 20:41:26 -0500

    when customer is created stripe id is automatically added

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,11 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_commit :assign_customer_id, on: :create
+
+  def assign_customer_id
+    customer = Stripe::Customer.create(email: email)
+    self.stripe_customer_id = customer.id
+  end
 end
