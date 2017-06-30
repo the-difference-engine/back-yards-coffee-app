a395bd0cf8706b9dc12d55f5b997fd56c06c8d86
Linda-Diamond-Robbins <lindadiamondrobbins@gmail.com>
2017-06-29 20:39:20 -0500

    fix customer and employee rspec

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -2,25 +2,19 @@ require 'rails_helper'
 
 
   RSpec.describe Employee, type: :model do
-    describe '#email' do
-    # before(:each) do
-    # end
-    
-    it 'should return the email in the model' do
-      employee = Employee.create(email: "employee@gmail.com")
-      expect(employee.email).to eq("employee@gmail.com")
-    end
-   
-     it "is invalid without an email address" do
-      employee = build(:employee, email: nil)
-      employee.valid?
-      expect(employee.errors[:email]).to include("can't be blank")
-     end
 
-     # it "needs a valid email address" do
-     #  employee = build(:employee, email: "joy")
-     #  expect(Employee.errors[:email]).to include("is missing")
-     # end  --This isn't working right now.
+    describe '#email' do
+    
+      it 'should return the email in the model' do
+        employee = Employee.create(email: "employee@gmail.com")
+        expect(employee.email).to eq("employee@gmail.com")
+      end
+     
+      it "is invalid without an email address" do
+        employee = build(:employee, email: nil)
+        employee.valid?
+        expect(employee.errors[:email]).to include("can't be blank")
+      end
 
     end
 
