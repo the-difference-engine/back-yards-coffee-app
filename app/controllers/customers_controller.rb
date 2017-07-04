51e14cdbcd5c75cd2c9dbb9d829551bb3612212d
Dillon Streator <dillonstreator@gmail.com>
2017-07-04 12:21:37 -0500

    controller variable for conditionally displaying extended-navbar on dashboard only

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,6 +3,7 @@ class CustomersController < ApplicationController
   before_action :current_customer
 
   def show
+    @dashboard = true
     @customer = Customer.find_by(id: current_customer.id)
   end
 end
