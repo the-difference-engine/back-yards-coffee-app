5fc009efdb931540cce35ec1f2ab3383e8f13151
emekdahl <emily.ekdahl@gmail.com>
2017-07-10 18:58:41 -0500

    carted products associations

--- spec/models/carted_product_spec.rb
+++ spec/models/carted_product_spec.rb
@@ -3,15 +3,6 @@ require 'rails_helper'
 RSpec.describe CartedProduct, type: :model do
 
 describe "CartedProduct" do 
-  it "is valid with user_id, product_id, quantity, status, grind_id, order_id" do 
-    carted_product = CartedProduct.new(
-      user_id: 1,
-      product_id: 1,
-      quantity: 1, 
-      status: "testing",
-      grind_id: 3, 
-      order_id: 100)
-    expect(carted_product).to be_valid
-  end 
 end
+
 end
