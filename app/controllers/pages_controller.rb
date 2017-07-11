2687a453a76e66d4d1b8365b8384b8f04a57b406
DillonStreator <dillonstreator@gmail.com>
2017-07-10 19:43:08 -0500

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
   def index
-    @products = Stripe::Product.list 
     @products = Stripe::Product.list.select{|product| product.attributes.include?('featured')}
     @subscriptions = Stripe::Plan.list
   end
