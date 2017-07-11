f09396fe09f3110f39f14cbd641d651c1f8bf38f
emekdahl <emily.ekdahl@gmail.com>
2017-07-10 19:29:21 -0500

    fix

--- spec/models/carted_product_spec.rb
+++ spec/models/carted_product_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe CartedProduct, type: :model do
-<<<<<<< HEAD
 
-describe "CartedProduct" do 
-end
 
-=======
->>>>>>> 85089ea3df5fc63b9695542aec67faa0de4e8166
 end
