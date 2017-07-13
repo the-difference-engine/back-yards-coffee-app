3a93ddb86b436c6bae24a6fc5d44502c619d8049
Dillon Streator <dillonstreator@gmail.com>
2017-07-13 10:47:56 -0500

    customer dashboard on one page with subscription and account info

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,7 +2,6 @@ class CustomersController < ApplicationController
   before_action :authenticate_customer!
 
   def show
-    @dashboard = true
     @customer = Stripe::Customer.retrieve(current_customer.stripe_customer_id)
   end
 end
