ebc7a0e52749bc03c421add3efca732de5332588
Dillon Streator <dillonstreator@gmail.com>
2017-07-19 21:35:08 -0500

    seperated components for customer search table and fixed header and table data binding

---
+++ app/assets/javascripts/components/customer_search_table.jsx
@@ -0,0 +1,16 @@
+var Table = React.createClass({
+  render: function(){
+    console.log("logging this..", this.props)
+    return (
+      <table>
+        <thead>
+          <tr className="row">
+            <td key="id" className="col s6 center">ID</td>
+            <td key="email" className="col s6 center">Email</td>
+          </tr>
+        </thead>
+        <CSTableRow data={this.props.items} />
+      </table>
+    );
+  }
+});
---
+++ app/assets/javascripts/components/customer_search_table_row.jsx
@@ -0,0 +1,14 @@
+var CSTableRow = React.createClass({
+  render() {
+    const{data} = this.props;
+    const row = data.map((data) =>
+      <tr key={data.id} className="row">
+        <td className="col s6 center">{data.id}</td>
+        <td className="col s6 center">{data.email}</td>
+      </tr>
+    );
+    return (
+      <tbody>{row}</tbody>
+    );
+  }
+})
