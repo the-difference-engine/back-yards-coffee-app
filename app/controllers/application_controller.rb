e3259c1607077f583477cc5929cb63a7bb1d2f2d
Jay Lockridge <jklockri@pnw.edu>
2017-07-27 20:41:00 -0500

    Add modal to sign in on add item to cart click

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,26 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   layout :layout_by_resource
+  after_filter :store_location
+
+def store_location
+  # store last url - this is needed for post-login redirect to whatever the user last visited.
+  return unless request.get? 
+    if (request.path != "/customers/sign_in" &&
+      request.path != "/customers/sign_up" &&
+      request.path != "/customers/password/new" &&
+      request.path != "/customers/password/edit" &&
+      request.path != "/customers/confirmation" &&
+      request.path != "/customers/sign_out" &&
+      !request.xhr?) 
+    session[:previous_url] = request.fullpath 
+   end
+  end
+
 
   #Overrides the default method for redirecting to home page given with devise. Simply returns the url/path.
   def after_sign_in_path_for(resource_or_scope)
-    "/#{resource_or_scope.class.name.downcase.pluralize}/dashboard"
+     session[:previous_url] || "/#{resource_or_scope.class.name.downcase.pluralize}/dashboard"
   end
 
   def guest_or_customer_id
