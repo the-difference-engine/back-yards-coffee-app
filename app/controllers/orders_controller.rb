4e8e2dee2235e5839ed1613d7a00efe6acdd6099
Rebecca Mutek <mutek.b@gmail.com>
2017-08-03 19:40:36 -0500

    orders page created with shipping

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,6 @@ class OrdersController < ApplicationController
 
     items = carted_products.map{ |o| {type: 'sku', parent: o.sku, quantity: o.quantity } }
 
-
     if customer_signed_in?
       customer = Stripe::Customer.retrieve(current_customer.stripe_customer_id)
     else
@@ -32,3 +31,15 @@ class OrdersController < ApplicationController
  end
 
 end
+# <h1>Order Overview</h1>
+
+# <% @order.items.each do |item| %>
+#   <h3>Type: <%= item.type%></h3>
+#   <p>Description: <%= item.description%></p>
+#   <p>Amount: <%= item.amount %></p>
+# <% end %>
+# <% @order.shipping_methods.each do |shipping_method| %>
+#   <p>Description: <%= shipping_method.description%></p>
+#   <p>Amount: <%= shipping_method.amount %></p>
+#   <p>Estimated Arrival Date: <%= shipping_method.delivery_estimate.date%></p>
+# <% end %>
