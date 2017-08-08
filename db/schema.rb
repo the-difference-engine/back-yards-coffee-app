b314af4132d58afdfd6d1a2835c62ce87c2e5a7e
JHughesSUTA <james.a.hughes@gmail.com>
2017-08-07 19:49:46 -0500

    Update schema.rb
    remove admin from schema

--- db/schema.rb
+++ db/schema.rb
@@ -60,7 +60,6 @@ ActiveRecord::Schema.define(version: 20170805162934) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.boolean  "admin"
     t.index ["email"], name: "index_employees_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true, using: :btree
   end
