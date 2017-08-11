cfac74db4b515471f27624b22ed8c815f06f326e
cdanmar <cdanmar@gmail.com>
2017-08-10 21:00:47 -0500

    added testing for customer carted products

--- spec/models/carted_product_spec.rb
+++ spec/models/carted_product_spec.rb
@@ -11,5 +11,16 @@ RSpec.describe CartedProduct, type: :model do
       carted_product = build(:carted_product, quantity: 0)
       expect(carted_product).not_to be_valid
     end
+    it 'is invalid with a non-numerical value' do
+      carted_product = build(:carted_product, quantity: "dog")
+      expect(carted_product).not_to be_valid
+    end
+    context "logged in as customer" do
+      it 'should return the customers carted products' do
+        customer = build(:customer, id: 123)
+        carted_product = create(:carted_product, status: 'carted', customer_id: customer.id)
+        expect(customer.carted_products.count).to eq(1)
+      end
+    end
   end
 end
