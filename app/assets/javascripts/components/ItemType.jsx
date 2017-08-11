588dc4728a93eb6ea42829f2fc28756a9ea6c8c0
James Hughes <james.a.hughes@gmail.com>
2017-08-10 21:01:35 -0500

    Order page to react

--- app/assets/javascripts/components/ItemType.jsx
+++ app/assets/javascripts/components/ItemType.jsx
@@ -1,12 +1,11 @@
-var ItemType = React.createClass({
-    getInitialState: function() {
-        return { 
-          order: this.props.order
-        };
-    }, 
-    render: function() {
-      return (
-
-        );
-    }
-});
+//var ItemType = React.createClass({
+//    getInitialState: function() {
+//        return { 
+//          order: this.props.order
+//        };
+//    }, 
+//    render: function() {
+//      return (
+//        );
+//    }
+//});
--- app/assets/javascripts/components/Order.jsx
+++ app/assets/javascripts/components/Order.jsx
@@ -8,26 +8,41 @@ var Order = React.createClass({
   componentWillMount: function(){
     this.setState({order: this.state.order})
   },
-  render: function() {
-      return (
-        <div>
-          <h3>Order Overview</h3>
-          {this.state.order.items.map(item =>
-                <div>
-                  <p>{item.type}: {item.description}</p>
-                  <p>Amount: {item.amount*0.01}</p>
-                </div>
-            )}
 
-        <h3> Shipping </h3>
-        {this.state.order.shipping_methods.map(shipping_method =>
-          <div>
-            <p>{shipping_method.description}</p>
-            <p>{shipping_method.amount*0.01}</p>
-            <p>{shipping_method.delivery_estimate.date}</p>
-          </div>
-        )}
+ formatItem: function(){
+    var test = [];
+    // return this.state.order.items.length;
+    this.state.order.items.map((item) => {
+      if (item.type === "sku") {
+        test.push(<ul>{item.description}</ul>);
+        test.push(<ul>{ (item.amount * 0.01).toLocaleString("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2})}</ul>);
+      } else if (item.type === "tax") {
+        test.push(<ul>Tax: { item.amount} </ul>);
+      } // else {
+      //   test.push(<ul>is a ship</ul>);
+      // }
+    });
+    return test;
 
+   // for (i = 0, i < this.order.items.length, i = i + 1) {
+    //   if (order.item.type === "sku") {
+    //     object =  <h2>this is a sku</h2>;
+    //   } else if {
+    //     (order.item.type === "tax") {
+    //       object =  <h2>this is tax</h2>;
+    //     }
+    //   }
+    // }
+  },
+
+ render: function() {
+      
+   return (
+      <div>
+        <h3>Review your Order</h3>
+        <ol>{this.formatItem()}</ol>
+        
       </div>
-    )}
+    )
+  }
 })
