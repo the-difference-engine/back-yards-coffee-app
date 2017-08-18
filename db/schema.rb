5a264157b2dcfc1e4fa374fc701836b0e3e7c4ac
Lynda Guillu <lynda_guillu@att.net>
2017-08-17 19:36:51 -0500

    UpdateCustomerSignUpForm

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170805162934) do
+ActiveRecord::Schema.define(version: 20170817235259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,10 @@ ActiveRecord::Schema.define(version: 20170805162934) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "stripe_customer_id"
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
+    t.integer  "zip_code"
     t.index ["email"], name: "index_customers_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true, using: :btree
   end
