e2f136e6819533661ac2ea13fff7a66c9befd6cc
cdanmar <cdanmar@gmail.com>
2017-08-17 19:32:46 -0500

    added product controller testing

--- db/schema.rb
+++ db/schema.rb
@@ -64,12 +64,4 @@ ActiveRecord::Schema.define(version: 20170805162934) do
     t.index ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true, using: :btree
   end
 
-  create_table "orders", force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "customer_id"
-    t.integer  "price"
-    t.integer  "total_price"
-  end
-
 end
