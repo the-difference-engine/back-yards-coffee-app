a5147f819423f5ca91d452cb4be1d8fea31facab
Jay Lockridge <jklockri@pnw.edu>
2017-08-21 19:09:53 -0500

    Made a larger limit for subs and products

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,13 @@
 class PagesController < ApplicationController
   def index
     @products = Stripe::Product.list.select{|product| product.attributes.include?('featured')}
-    @subscriptions = Stripe::Plan.list
+    @subscriptions = Stripe::Plan.list(limit: 50)
   end
   def about
   end
   def coffee_club
     @products = Stripe::Product.list.select{|product| product.attributes.include?('featured')}
-    @subscriptions = Stripe::Plan.list
+    @subscriptions = Stripe::Plan.list(limit: 50)
   end
   def coffee_house
   end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,16 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Stripe::Product.list
+    @products = Stripe::Product.list(limit: 50)
   end
 
   def show
-    @subscriptions=Stripe::Plan.list
+    @subscriptions=Stripe::Plan.list(limit: 50)
     @product = Stripe::Product.retrieve(id: params[:id])
   end
 
   def subscriptions
-    @subscriptions=Stripe::Plan.list
+    @subscriptions=Stripe::Plan.list(limit: 50)
     p "SUBSCRIPTIONS #{@subscriptions}"
   end
 end
