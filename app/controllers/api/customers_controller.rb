16f5b9f4010ebd93992d5beb4a0202f664fc7c6b
“charliedayman” <leifenesse@gmail.com>
2017-09-11 20:29:45 -0500

    added update to shipping address for shipping calculation

--- app/controllers/api/customers_controller.rb
+++ app/controllers/api/customers_controller.rb
@@ -5,18 +5,42 @@ class Api::CustomersController < ApplicationController
   end
 
   def update
-    p params
-
     @customer = Customer.find_by(id: params[:id])
 
-    if @customer.update(first_name: params[:first_name],
-                        last_name: params[:last_name],
-                        address: params[:address],
-                        Address2: params[:Address2],
-                        zip_code: params[:zip_code],
-                        city: params[:city],
-                        state: params[:state])
-      render json: @customer
+    if @customer.update(first_name: params[:address][:first_name],
+                        last_name: params[:address][:last_name],
+                        address: params[:address][:address],
+                        Address2: params[:address][:Address2],
+                        zip_code: params[:address][:zip_code],
+                        city: params[:address][:city],
+                        state: params[:address][:state])
+
+    # if customer_signed_in?
+    #   customer = Stripe::Customer.retrieve(current_customer.stripe_customer_id)
+    # else
+    #   customer = Stripe::Customer.create(:email  => params[:stripeEmail])
+    # end
+
+    carted_products = CartedProduct.my_carted(guest_or_customer_id)
+
+    items = carted_products.map{ |o| {type: 'sku', parent: o.sku, quantity: o.quantity } }
+
+    order = Stripe::Order.create(
+      :currency => 'usd',
+      :customer => @customer.stripe_customer_id,
+      :items => items,
+      :shipping => {
+        :name => @customer.first_name,
+        :address => {
+          :line1 => @customer.address,
+          :city => @customer.city,
+          :country => 'US',
+          :postal_code => @customer.zip_code
+        }
+      },
+    )
+
+      render json: order
     else
       render json: @customer.errors.full_messages
     end
