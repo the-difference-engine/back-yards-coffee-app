35c5df85c8c1bfb99875c9ebc9ed9b2d2186730e
Daniel Hey <danhey@Daniel-Hey.local>
2017-09-15 17:27:26 -0500

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,130 +1,142 @@
-Product.create(
+Category.create!(
+  name:"Coffees",
+)
+
+Category.create!(
+  name:"Espressos",
+)
+
+Category.create!(
+  name:"Iced",
+)
+
+Product.create!(
   name:"47th Street Drip Coffee 12oz",
   category_id:1,
   description:"Our signature blend",
   price:2
 )
 
-Product.create(
+Product.create!(
   name:"Cafe de Olla 12oz",
   category_id:1,
   description:"47th Street Blend infused with Mexican raw sugar molasses and spiced with cinnamon, star anise, and clove.",
   price: 3
 )
 
-Product.create(
+Product.create!(
   name:"Cafe con Leche 12oz",
   category_id:1,
   description:"Coffee and steamed milk",
   price: 3
 )
 
-Product.create(
+Product.create!(
   name:"Pour Over",
   category_id:1,
   description:"Add a description about this item",
   price: 4
 )
 
-Product.create(
+Product.create!(
   name:"Chemex for two",
   category_id:1,
   description:"Add a description about this item",
   price: 5
 )
 
-Product.create(
+Product.create!(
   name:"Xocolatte",
   category_id:2,
   description:"Espresso, chocolate, cinnamon, chile ancho, steamed milk",
   price: 3.75
 )
 
-Product.create(
+Product.create!(
   name:"Ojo Rojo",
   category_id:2,
   description:"Cafe de Olla and espresso",
   price: 3.50
 )
 
-Product.create(
+Product.create!(
   name:"Cortadito",
   category_id:2,
   description:"Add a description about this item",
   price: 2.50
 )
 
-Product.create(
+Product.create!(
   name:"Cubanito",
   category_id:2,
   description:"Add a description about this item",
   price: 2.50
 )
 
-Product.create(
+Product.create!(
   name:"Cappuccino",
   category_id:2,
   description:"Add a description about this item",
   price: 3
 )
 
-Product.create(
+Product.create!(
   name:"Latte",
   category_id:2,
   description:"Add a description about this item",
   price: 3.5
 )
 
-Product.create(
+Product.create!(
   name:"Mocha",
   category_id:2,
   description:"Add a description about this item",
   price: 3.5
 )
 
-Product.create(
+Product.create!(
   name:"Shot of Espresso",
   category_id:2,
   description:"Add a description about this item",
   price: 2
 )
 
-Product.create(
+Product.create!(
   name:"Decaf Americano",
   category_id:2,
   description:"Add a description about this item",
   price: 2.5
 )
 
-Product.create(
+Product.create!(
   name:"El Toro Cold Brew",
   category_id:3,
   description:"Cold brew served over ice",
   price: 3.5
 )
 
-Product.create(
+Product.create!(
   name:"Toro Loco",
   category_id:3,
   description:"47th Street Cold Brew, CO2 bubbles, simple syrup and lime",
   price: 4
 )
 
-Product.create(
+Product.create!(
   name:"Horchatta Latte",
   category_id:3,
   description:"47th Street Cold brew mixed with housemade rice syrup, spiced with cinnamon",
   price: 3.5
 )
 
-Product.create(
+Product.create!(
   name:"Dulce de Leche",
   category_id:3,
   description:"47th Street Cold Brew and Lecherita Creamer Syrup",
   price: 3.5
 )
 
-Product.create(
+Product.create!(
   name:"Iced Cafe de Olla",
   category_id:3,
   description:"47th Street Cold Brew infused with piloncillo, cinnamon, star anise, clavo",
