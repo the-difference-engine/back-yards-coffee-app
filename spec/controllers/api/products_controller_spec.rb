97d7988c7abede3f30704eaa275eaba6ebddc745
cdanmar <cdanmar@gmail.com>
2017-09-25 18:22:27 -0500

    added check to see if API would render

--- spec/controllers/api/products_controller_spec.rb
+++ spec/controllers/api/products_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Api::ProductsController, type: :controller do
-
+  describe "GET api/products#index" do
+    it "should respond with a status of 200" do
+      get :index, format: :json
+      expect(response.status).to eq 200
+    end
+  end
 end
