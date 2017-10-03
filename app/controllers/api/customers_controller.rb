982a75e43e26a2179e97a72ced8990a17bc0d84d
James Hughes <james.a.hughes@gmail.com>
2017-10-02 20:53:49 -0500

    Update some syntax, clear some whitespace

--- app/controllers/api/customers_controller.rb
+++ app/controllers/api/customers_controller.rb
@@ -7,18 +7,18 @@ class Api::CustomersController < ApplicationController
   def update
     @customer = Customer.find_by(id: params[:id])
 
-    full_name = params[:address][:first_name] + ' ' + params[:address][:last_name]
+    full_name = "#{params[:address][:first_name]} #{params[:address][:last_name]}"
 
-    begin 
+    begin
       address_from = Shippo::Address.create(
-        :name => full_name, 
-        :street1 => params[:address][:address],
-        :street2 => params[:address][:Address2],
-        :city => params[:address][:city],
-        :state => params[:address][:state],
-        :zip => params[:address][:zip_code],
-        :country => "US",    
-        :validate => true
+        name: full_name,
+        street1: params[:address][:address],
+        street2: params[:address][:Address2],
+        city: params[:address][:city],
+        state: params[:address][:state],
+        zip: params[:address][:zip_code],
+        country: 'US',
+        validate: true
       )
 
       if address_from.validation_results.is_valid
@@ -27,13 +27,13 @@ class Api::CustomersController < ApplicationController
           address2: address_from.street2,
           city:  address_from.city,
           zip_code: address_from.zip,
-          state: address_from.state,
+          state: address_from.state
         )
       else
         render json: address_from
       end
     rescue
-      render json: { error: "Shippo API error", code: 500 }
+      render json: { error: 'Shippo API error', code: 500 }
     end
 
   end
