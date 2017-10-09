f6b23a5eb12a37da87714ca57fcce1eb9a99ac13
Jonathan Martinez <jonathan.martinez1217@gmail.com>
2017-10-09 18:46:44 -0500

    spec categories controller

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe RegistrationsController, type: :controller do
-
+  describe "Sign Up Params" do 
+    it "valid sing up parrams" do 
+      delete :destroy, id: @category
+      expect(response).to redirect_to('/coffee_house')
+    end
+  end
 end
