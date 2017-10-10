31f6e9fdd54aa97f595cc499915bdd87d05d3291
James Hughes <james.a.hughes@gmail.com>
2017-10-09 20:48:53 -0500

    make more changes according to style-guide

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,18 +1,20 @@
 class ChargesController < ApplicationController
   def create
-    carted_products = CartedProduct.my_carted(guest_or_customer_id)
     email = current_customer ? current_customer.email : params[:stripeEmail]
     order = Stripe::Order.retrieve(params[:order_id])
     token = params[:stripeToken]
-    order.pay(source: token, email: email)
+
+    begin
+      order.pay(source: token, email: email)
     rescue Stripe::CardError => e
       flash[:error] = e.message
       redirect_to '/cart'
-    else
-      carted_products.each do |carted_product| carted_product.status = "product ordered"
-      carted_product.save
     end
-    flash[:success] = "Charge created!"
+
+    carted_products = CartedProduct.my_carted(guest_or_customer_id)
+    carted_products.map { |carted_product| carted_product.status = 'product ordered' }
+    carted_product.save
+    flash[:success] = 'Charge created!'
     redirect_to '/'
   end
 end
