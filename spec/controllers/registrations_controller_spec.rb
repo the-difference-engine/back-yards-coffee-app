117b95fcf39cf1ea29d52a2e94b11a049565a849
Jonathan Martinez <jonathan.martinez1217@gmail.com>
2017-10-09 19:15:53 -0500

    remove registration controller spec

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -1,11 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe RegistrationsController, type: :controller do
-
-  describe "Sign Up Params" do 
-    it "valid sing up parrams" do 
-      delete :destroy, id: @category
-      expect(response).to redirect_to('/coffee_house')
-    end
-  end
 end
