df3aeb5f45137874253630577c64e71049a5ae91
Jonathan Martinez <jonathan.martinez1217@gmail.com>
2017-10-19 19:17:46 -0500

    adds all customers button to dropdown

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,16 +1,10 @@
 class CustomersController < ApplicationController
   before_action :authenticate_customer!, except: [:index]
   before_action :authenticate_employee_admin!, only: [:index]
-
   def index
     @customers = Customer.all
-
   end
   def show
     @customer = Stripe::Customer.retrieve(current_customer.stripe_customer_id)
-  end
-
-  def index
-    render 'index.html.erb'
   end
 end
