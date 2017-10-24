422bf40878a3d7e2166f742011f7798a3a3ce0e9
Jay Lockridge <jklockri@pnw.edu>
2017-10-23 20:54:18 -0500

    Made products show form better

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,27 +1,59 @@
 class CartedProductsController < ApplicationController
   def create
-    carted_product = CartedProduct.find_by(status: 'carted',
-                                            customer_id: guest_or_customer_id,
-                                            sku: params[:sku])
-    if carted_product
-      carted_product.quantity = carted_product.quantity.to_i + params[:quantity].to_i
-    else
-      carted_product = CartedProduct.new(quantity: params[:quantity],
-                                          product_id: params[:product_id],
-                                          sku: params[:sku],
-                                          customer_id: guest_or_customer_id,
-                                          status: 'carted',
-                                          price: params[:price].to_i,
-                                          name: params[:name])
-    end
 
-    if carted_product.save
-      flash[:success] = 'Product Added to Cart!'
-      redirect_to '/cart'
+    if params[:plan_id] == "One Time Buy"
+      carted_product = CartedProduct.find_by(status: 'carted',
+                                              customer_id: guest_or_customer_id,
+                                              sku: params[:sku])
+        if carted_product
+          carted_product.quantity = carted_product.quantity.to_i + params[:quantity].to_i
+        else
+          carted_product = CartedProduct.new(quantity: params[:quantity],
+                                              product_id: params[:product_id],
+                                              sku: params[:sku],
+                                              customer_id: guest_or_customer_id,
+                                              status: 'carted',
+                                              price: params[:price].to_i,
+                                              name: params[:name])
+        end
+        if carted_product.save
+          flash[:success] = 'Product Added to Cart!'
+          redirect_to '/cart'
+        else
+          flash[:error] = carted_product.errors.full_messages.join(', ').gsub(/[']/,"\\\\\'")
+          redirect_to "/products/#{params[:product_id]}"
+        end
+
     else
-      flash[:error] = carted_product.errors.full_messages.join(', ').gsub(/[']/,"\\\\\'")
-      redirect_to "/products/#{params[:product_id]}"
-    end
+      carted_subscription = CartedSubscription.find_by(status: 'carted',
+                                              customer_id: guest_or_customer_id,
+                                              plan_id: params[:plan_id],
+                                              grind: params[:grind])
+      if carted_subscription
+        carted_subscription.quantity = carted_subscription.quantity.to_i + params[:quantity].to_i
+      else
+        plans = Stripe::Plan.list(limit: 50)
+        plan = StripeTool.find_plan(plans, params[:plan_id], params[:product_id])
+        carted_subscription = CartedSubscription.new(quantity: params[:quantity],
+                                            customer_id: guest_or_customer_id,
+                                            status: 'carted',
+                                            plan_id: params[:plan_id],
+                                            grind: params[:grind],
+                                            amount: plan[0].amount,
+                                            interval: plan[0].interval,
+                                            interval_count: plan[0].interval_count)
+      end
+      if carted_subscription.save
+        flash[:success] = 'Product Added to Cart!'
+        redirect_to '/cart'
+      else
+        flash[:error] = carted_subscription.errors.full_messages.join(', ').gsub(/[']/,"\\\\\'")
+        redirect_to "/products/#{params[:product_id]}"
+      end
+
+  end
+
+
   end
 
   def index
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,7 @@ class ProductsController < ApplicationController
     @subscriptions=Stripe::Plan.list(limit: 50)
     @product = Stripe::Product.retrieve(id: params[:id])
     @product_plan_options = StripeTool.product_plan_options(@subscriptions, @product.id)
+    @product_plan_options.push("One Time Buy")
   end
 
   def subscriptions
