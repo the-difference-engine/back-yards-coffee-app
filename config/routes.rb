9990b1cae6a07041981e4f284b29774d119a2baf
Arun Madisetty <arun.madisetty@gmail.com>
2017-10-30 18:52:32 -0500

    quieted the barking hounds

--- config/routes.rb
+++ config/routes.rb
@@ -36,8 +36,8 @@ Rails.application.routes.draw do
 
   post '/carted_subscription' => 'carted_subscriptions#create'
 
-  get "/orders/new" => "orders#new"
-  get "/orders/:id" => "orders#show"
+  get '/orders/new' => 'orders#new'
+  get '/orders/:id' => 'orders#show'
 
   resources :products
   get '/subscriptions' => 'products#subscriptions'
