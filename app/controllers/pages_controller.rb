acfcc64fc0db421ecfcac91f2703df89fa5321a0
“charliedayman” <leifenesse@gmail.com>
2017-10-29 23:41:56 -0500

    moved contact info in footer to seperate page and updated background colors to fit color scheme

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,15 +3,24 @@ class PagesController < ApplicationController
     @products = Stripe::Product.list.select{|product| product.attributes.include?('featured')}
     @subscriptions = Stripe::Plan.list(limit: 50)
   end
+
   def about
   end
+
   def coffee_club
     @products = Stripe::Product.list.select{|product| product.attributes.include?('featured')}
     @subscriptions = Stripe::Plan.list(limit: 50)
   end
+
   def coffee_house
     @categories=Category.all
   end
 
+  def contact
+    render 'contact.html.erb'
+  end
 
+  def faqs
+    render 'faqs.html.erb'
+  end
 end
--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,2 +1,30 @@
 class SubscriptionsController < ApplicationController
+  def new
+    subscription = Subscription.find_by(status: 'carted',
+                                            customer_id: customer_id,
+                                            plan_id: params[:plan_id],
+                                            grind: params[:grind])
+    if subscription
+      subscription.quantity = subscription.quantity.to_i + params[:quantity].to_i
+    else
+      plans = Stripe::Plan.list(limit: 50)
+      plan = StripeTool.find_plan(plans, params[:plan_id], params[:product_id])
+      subscription = Subscription.new(quantity: params[:quantity],
+                                          customer_id: customer_id,
+                                          status: 'carted',
+                                          plan_id: params[:plan_id],
+                                          grind: params[:grind],
+                                          amount: plan[0].amount,
+                                          interval: plan[0].interval,
+                                          interval_count: plan[0].interval_count)
+    end
+
+    if subscription.save
+      flash[:success] = 'Subscription Added to Cart!'
+      redirect_to '/cart'
+    else
+      flash[:error] = subscription.errors.full_messages.join(', ').gsub(/[']/,"\\\\\'")
+      redirect_to "/products/#{params[:product_id]}"
+    end
+  end
 end
