7754432b0dccefc1aee334e50ad5f1c5fc2ef812
Daniel Hey <danhey@Daniel-Hey.local>
2017-11-06 10:50:01 -0600

    added redirect for anon customer trying to create a subscription

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,6 +1,11 @@
 class SubscriptionsController < ApplicationController
   def new
-    @customer = current_customer
-    @carted_subscriptions = @customer.carted_subscriptions
+    if current_customer
+      @customer = current_customer
+      @carted_subscriptions = @customer.carted_subscriptions
+    else
+      flash['warning'] = 'You must be have an account to create a subscription.'
+      redirect_to new_customer_registration_path
+    end
   end
 end
