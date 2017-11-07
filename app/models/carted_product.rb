2322d5abe32f8a3d45e84c638eb1dcc2871efa76
Rebecca Mutek <mutek.b@gmail.com>
2017-11-06 20:32:32 -0600

    errors fixed

--- app/models/carted_product.rb
+++ app/models/carted_product.rb
@@ -1,8 +1,8 @@
 class CartedProduct < ApplicationRecord
   belongs_to :customer, optional: true
   validates :quantity, :product_id, :customer_id, :price, :status, presence: true
-  validates :sku, presence: true, message: 'Please enter a bean style'
-  validates :name, presence: true, message: 'Please enter a plan'
+  validates :sku, presence: { message: 'Please enter a bean style' }
+  validates :name, presence: { message: 'Please enter a plan' }
   validates :quantity, numericality: { greater_than: 0 }
   belongs_to :order, optional: true
 
