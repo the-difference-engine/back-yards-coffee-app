6d08e8552b3445864b00d0cc52bfda41b82c2f8e
Rebecca Mutek <mutek.b@gmail.com>
2017-11-06 18:13:55 -0600

    skufix done

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,64 +1,66 @@
 class CartedProductsController < ApplicationController
   def create
-    if params[:plan_id] == 'One Time Buy'
-      carted_product = CartedProduct.find_by(
-        status: 'carted',
-        customer_id: guest_or_customer_id,
-        sku: params[:sku]
-      )
-      if carted_product
-        carted_product.quantity = carted_product.quantity.to_i + params[:quantity].to_i
-      else
-        carted_product = CartedProduct.new(
-          quantity: params[:quantity],
-          product_id: params[:product_id],
-          sku: params[:sku],
-          customer_id: guest_or_customer_id,
-          status: 'carted',
-          price: params[:price].to_i,
-          name: params[:name]
-        )
-      end
-      if carted_product.save
-        flash[:success] = 'Product Added to Cart!'
-        redirect_to '/cart'
-      else
-        flash[:error] = carted_product.errors.full_messages.join(', ').gsub(/[']/, "\\\\\'")
-        redirect_to "/products/#{params[:product_id]}"
-      end
 
+  # checks plan id first; if plan id indicates one time purchase then directed to carted products checkout process
+  if params[:plan_id] == 'One Time Buy'
+    carted_product = CartedProduct.find_by(
+      status: 'carted',
+      customer_id: guest_or_customer_id,
+      sku: params[:sku]
+    )
+    if carted_product
+      carted_product.quantity = carted_product.quantity.to_i + params[:quantity].to_i
     else
-      carted_subscription = CartedSubscription.find_by(
-        status: 'carted',
+      carted_product = CartedProduct.new(
+        quantity: params[:quantity],
+        product_id: params[:product_id],
+        sku: params[:sku],
         customer_id: guest_or_customer_id,
-        plan_id: params[:plan_id],
-        grind: params[:grind]
+        status: 'carted',
+        price: params[:price].to_i,
+        name: params[:name]
       )
-      if carted_subscription
-        carted_subscription.quantity = carted_subscription.quantity.to_i + params[:quantity].to_i
-      else
-        plans = Stripe::Plan.list(limit: 50)
-        plan = StripeTool.find_plan(plans, params[:plan_id], params[:product_id])
-        carted_subscription = CartedSubscription.new(
-          quantity: params[:quantity],
-          customer_id: guest_or_customer_id,
-          status: 'carted',
-          plan_id: params[:plan_id],
-          grind: params[:grind],
-          amount: plan[0].amount,
-          interval: plan[0].interval,
-          interval_count: plan[0].interval_count
-        )
-      end
-      if carted_subscription.save
-        flash[:success] = 'Product Added to Cart!'
-        redirect_to '/cart'
-      else
-        flash[:error] = carted_subscription.errors.full_messages.join(', ').gsub(/[']/, "\\\\\'")
-        redirect_to "/products/#{params[:product_id]}"
-      end
+    end
+    if carted_product.save
+      flash[:success] = 'Product Added to Cart!'
+      redirect_to '/cart'
+    else
+      flash[:error] = carted_product.errors.values.join(', ').gsub(/[']/, "\\\\\'")
+      redirect_to "/products/#{params[:product_id]}"
+    end
+  # if plan id is determined to not be a one time purchase a stripe plan id is assigned and checkout is directed to carted subscriptions
+  else
+    carted_subscription = CartedSubscription.find_by(
+      status: 'carted',
+      customer_id: guest_or_customer_id,
+      plan_id: params[:plan_id],
+      grind: params[:grind]
+    )
+    if carted_subscription
+      carted_subscription.quantity = carted_subscription.quantity.to_i + params[:quantity].to_i
+    else
+      plans = Stripe::Plan.list(limit: 50)
+      plan = StripeTool.find_plan(plans, params[:plan_id], params[:product_id])
+      carted_subscription = CartedSubscription.new(
+        quantity: params[:quantity],
+        customer_id: guest_or_customer_id,
+        status: 'carted',
+        plan_id: params[:plan_id],
+        grind: params[:grind],
+        amount: plan[0].amount,
+        interval: plan[0].interval,
+        interval_count: plan[0].interval_count
+      )
+    end
+    if carted_subscription.save
+      flash[:success] = 'Product Added to Cart!'
+      redirect_to '/cart'
+    else
+      flash[:error] = carted_subscription.errors.full_messages.join(', ').gsub(/[']/, "\\\\\'")
+      redirect_to "/products/#{params[:product_id]}"
     end
   end
+end
 
   def index
     @carted_products = CartedProduct.my_carted(guest_or_customer_id)
--- app/models/carted_product.rb
+++ app/models/carted_product.rb
@@ -1,6 +1,8 @@
 class CartedProduct < ApplicationRecord
   belongs_to :customer, optional: true
-  validates_presence_of :quantity, :product_id, :sku, :customer_id, :price, :status
+  validates_presence_of :quantity, :product_id, :customer_id, :price, :status
+  validates_presence_of :sku, message: "Please enter a bean style"
+  validates_presence_of :name, message: "Please enter a plan"
   validates :quantity, numericality: { greater_than: 0 }
   belongs_to :order, optional: true
 
