4083a4d112c3a026991b37aff6c4841afb1aea2e
Timothy "Manu" Meixell <misternu@gmail.com>
2017-12-01 00:16:45 -0600

    Revert "Prevent customers from editing wholesalers"

--- app/controllers/wholesalers_controller.rb
+++ app/controllers/wholesalers_controller.rb
@@ -1,5 +1,5 @@
 class WholesalersController < ApplicationController
-  before_action :authenticate_employee!, except: [:index]
+  before_action :authenticate_customer_or_employee!, except: [:index]
   def index
     @apps = Wholesaler.all
   end
