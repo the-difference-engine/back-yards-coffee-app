c69e0871dce3133270a9038cdf0774a839c0fb81
Timothy Meixell <misternu@gmail.com>
2017-12-04 14:45:46 -0600

    Add api/employee_controller specs

--- spec/controllers/api/employees_controller_spec.rb
+++ spec/controllers/api/employees_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Api::EmployeesController, type: :controller do
-
+  describe 'GET api/employees#index' do
+    render_views
+    before :each do
+      employee = create(:employee)
+      get :index
+    end
+    it 'assigns the employees to @employees' do
+      expect(assigns(:employees)).to eq Employee.all
+    end
+    it 'renders the index.json.jbuilder template' do
+      expect(response).to render_template 'index.json.jbuilder'
+    end
+    it 'responds with type JSON' do
+      expect(response.content_type).to eq "application/json"
+    end
+    it 'renders the correct JSON' do
+      json = JSON.parse(response.body)
+      expect(json.first.keys).to eq ["id", "email"]
+      expect(json.first["email"]).to match /employeedoe\d@example\.com/
+    end
+  end
 end
