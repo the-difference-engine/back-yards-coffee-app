874d5cca2e720b4aca632f939717fc1dbd41d2c4
“AustWick” <austin.wicketts@gmail.com>
2017-12-11 20:38:40 -0600

    fixed alot of stuffs

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe CustomersController, type: :controller do
     end
     it 'redirects to edit page' do
       customer = create(:customer)
-      get :edit, id: customer
+      get :edit, params: {id: customer}
       expect(response).to render_template :edit
     end
   end
@@ -38,38 +38,29 @@ RSpec.describe CustomersController, type: :controller do
       sign_in @customer
     end
     it 'creates a flash message: Unable to update address' do
-      patch :update, id: @customer
+      patch :update, params: {id: @customer}
       expect(flash[:warning]).to be_present
     end
 
     it "locates the requested @customer" do
-      patch :update, id: @customer, customer: attributes_for(:customer)
+      patch :update, params: { id: @customer }
       expect(assigns(:customer)).to eq(@customer)
     end
 
     it "changes @customer attributes" do
-      patch :update, id: @customer,
+      patch :update, params: {id: @customer}
       @customer.update(address: "meow")
-
-
-      p "QQQQQQQQQQQQ"
-      p @customer
-      p "QQQQQQQQQQQQ"
       expect(@customer.address).to eq('meow')
     end
 
-    xit "should redirect to '/customers/dashboard'" do
-      p "QQQQQQQQQQQQ"
-      p @customer
-      p "QQQQQQQQQQQQ"
-
-      patch :update, id: @customer.id
-      @customer.update(address: "meow")
-      @customer.save
-      expect(response).to redirect_to :dashboard
+    it "should redirect to '/customers/dashboard'" do
+      patch :update, params: attributes_for(:customer).merge({
+          id: @customer.id,
+          address: "meow",
+          zip_code: '90210'
+        })
+      expect(response).to redirect_to '/customers/dashboard'
+      expect(Customer.find(@customer.id).address).to eq "meow"
     end
-
-
-
   end
 end
