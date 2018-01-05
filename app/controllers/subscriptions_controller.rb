7c8d1536603d2473f538c072ef4a719ab1d8a40b
Timothy Meixell <misternu@gmail.com>
2018-01-04 19:54:54 -0600

    Change subscriptions checkout to only show subscriptions with status 'carted'

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,8 +1,9 @@
 class SubscriptionsController < ApplicationController
+  before_action :authenticate_customer!
   def new
     if current_customer
       @customer = current_customer
-      @carted_subscriptions = @customer.carted_subscriptions
+      @carted_subscriptions = CartedSubscription.my_carted(@customer.id)
     else
       flash['warning'] = 'You must be have an account to create a subscription.'
       redirect_to new_customer_registration_path
