da81cdaa24031e53fab5e3fd2ec4fc53af74f329
Jay Lockridge <jklockri@pnw.edu>
2018-02-18 10:29:41 -0600

    Made subtotal dynamic

--- app/assets/javascripts/carted_products_index.js
+++ app/assets/javascripts/carted_products_index.js
@@ -5,6 +5,7 @@ $('.carted_products.index').ready(function(){
   var deleteProductBoxes = document.querySelectorAll('.deleteProducts');
   var deleteSubscriptionBoxes = document.querySelectorAll('.deleteSubscriptions');
   var button = document.querySelector('.addsAjax');
+  var subTotal = document.querySelector('.totalFoSho');
   const cartedProducts = gon.cartedProducts
   const cartedSubscriptions = gon.cartedSubscriptions
 
@@ -42,12 +43,9 @@ $('.carted_products.index').ready(function(){
 
 
   function grabProductAmount() {
-
     for (var i = 0; i < inputBoxesProducts.length; i++) {
-
       if (cartedProducts[i].id === parseInt(inputBoxesProducts[i].id.slice(2))) {
         cartedProducts[i].amount = inputBoxesProducts[i].value
-
         $.ajax({
           type: "PATCH",
           url: "/api/carted_products/" + cartedProducts[i].id + "/" + cartedProducts[i].amount,
@@ -64,8 +62,6 @@ $('.carted_products.index').ready(function(){
   }
 
   function grabSubscriptionAmount() {
-
-
     for (var i = 0; i < inputBoxesSubscriptions.length; i++) {
 
       if (cartedSubscriptions[i].id === parseInt(inputBoxesSubscriptions[i].id.slice(2))) {
@@ -98,7 +94,6 @@ $('.carted_products.index').ready(function(){
 
     function deleteSubscription(){
       const id = this.id.slice(2);
-      console.log(id);
       $.ajax({
         type: "DELETE",
         url: "/api/carted_subscriptions/" + id,
@@ -108,50 +103,54 @@ $('.carted_products.index').ready(function(){
     }
 
     function subTotalView() {
-
       var itemsTotal = 0;
-
       for (var i = 0; i < cartedProducts.length; i++){
         itemsTotal = parseInt(cartedProducts[i].price * cartedProducts[i].quantity) + itemsTotal;
       }
       for (var i = 0; i < cartedSubscriptions.length; i++){
-        console.log(cartedSubscriptions[i].quantity)
-
         itemsTotal = parseInt(cartedSubscriptions[i].amount * cartedSubscriptions[i].quantity) + itemsTotal;
       }
+      subTotal.innerText= "Subtotal: $" + itemsTotal * 0.01;
       return "Subtotal: $" + itemsTotal * 0.01;
     }
 
-    function checking(){
-      // console.log(this.parentNode.firstChild.nextSibling.value);
-      for (var i = 0; i < inputBoxesSubscriptions.length; i++) {
-        if( cartedSubscriptions[i].id === parseInt(inputBoxesSubscriptions[i].id.slice(2))){
-          cartedSubscriptions[i].quantity = this.parentNode.firstChild.nextSibling.value;
-          subTotalView();
+    const quantityButtons = document.querySelectorAll('.quantity-nav');
+
+    function updateQuantity(){
+      var newQuantity = this.parentNode.firstChild.nextSibling.value
+      for(var i = 0;i < cartedSubscriptions.length;i++){
+        if(parseInt(this.parentNode.firstChild.nextSibling.id.slice(2)) === cartedSubscriptions[i].id){
+          cartedSubscriptions[i].quantity = newQuantity;
         }
       }
+      for(var i = 0;i < cartedProducts.length;i++){
+        if(parseInt(this.parentNode.firstChild.nextSibling.id.slice(2)) === cartedProducts[i].id){
+          cartedProducts[i].quantity = newQuantity;
+        }
+      }
+      subTotalView();
     }
 
-  const quantityButtons = document.querySelectorAll('.quantity-nav');
-  console.log(quantityButtons);
+
 
   for (var i = 0; i < deleteProductBoxes.length; i++){
     deleteProductBoxes[i].addEventListener('click', deleteProduct);
-    inputBoxesProducts[i].addEventListener('blur',checking);
+    inputBoxesProducts[i].addEventListener('blur',updateQuantity);
   }
 
   for (var i = 0; i < deleteSubscriptionBoxes.length; i++){
     deleteSubscriptionBoxes[i].addEventListener('click', deleteSubscription)
-    inputBoxesSubscriptions[i].addEventListener('blur',checking);
+    inputBoxesSubscriptions[i].addEventListener('blur',updateQuantity);
   }
 
   for (var i = 0; i< quantityButtons.length; i++){
-    quantityButtons[i].addEventListener('click',checking);
+    quantityButtons[i].addEventListener('click',updateQuantity);
   }
 
 
   button.addEventListener('click', grabProductAmount);
   button.addEventListener('click', grabSubscriptionAmount);
-  document.querySelector('.totalFoSho').innerHTML = subTotalView();
+  subTotal.innerText = subTotalView();
+
 
   })
