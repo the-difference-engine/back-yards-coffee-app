05008babaf4a26894e2e9b2a39fd47207dcf0c0d
Jay Lockridge <jklockri@pnw.edu>
2018-02-19 18:02:41 -0600

    made decimal only 2 places

--- app/assets/javascripts/carted_products_index.js
+++ app/assets/javascripts/carted_products_index.js
@@ -13,12 +13,12 @@ $('.carted_products.index').ready(function () {
 
   jQuery('<div class="quantity-nav"><div class="quantity-button quantity-up">+</div><div class="quantity-button quantity-down">-</div></div>').insertAfter('.quantity input');
   jQuery('.quantity').each(function () {
-    var spinner = jQuery(this),
-      input = spinner.find('input[type="number"]'),
-      btnUp = spinner.find('.quantity-up'),
-      btnDown = spinner.find('.quantity-down'),
-      min = input.attr('min'),
-      max = input.attr('max');
+    var spinner = jQuery(this);
+    var  input = spinner.find('input[type="number"]');
+    var  btnUp = spinner.find('.quantity-up');
+    var  btnDown = spinner.find('.quantity-down');
+    var min = input.attr('min');
+    var max = input.attr('max');
 
     btnUp.click(function () {
       var oldValue = parseFloat(input.val());
@@ -131,8 +131,8 @@ $('.carted_products.index').ready(function () {
       itemsTotal = parseInt(cartedSubscriptions[i].amount * cartedSubscriptions[i].quantity) + itemsTotal;
     }
 
-    subTotal.innerText = 'Subtotal: $ ' + itemsTotal * 0.01;
-    return 'Subtotal: $' + itemsTotal * 0.01;
+    subTotal.innerText = 'Subtotal: $ ' + (itemsTotal * 0.01).toFixed(2);
+    return 'Subtotal: $' + (itemsTotal * 0.01).toFixed(2);
   }
 
   const quantityButtons = document.querySelectorAll('.quantity-nav');
