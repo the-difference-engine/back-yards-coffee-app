736a93854741a673df3c729f6afaf04447c9e5f0
Timothy Meixell <misternu@gmail.com>
2018-03-26 19:21:29 -0500

    Add spec for admin button

--- spec/features/admin_features_spec.rb
+++ spec/features/admin_features_spec.rb
@@ -1,17 +1,25 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.feature "Admin dashboard" do
-    # context 'visit admin page before logging in'
-    #     before :each do
-    #         admin = create(:admin)
-    #         # employee = create(:employee)
-    #         # customer = create(:customer)
-    #     end
-    #     it 'not logged in' do
-    #         visit '/admin'
-    #     end
-        # it 'logs in as admin' do
-        #     visit '/admin'
-        #     expect(current_path).to eq(admin_root_path)
-        # end
+RSpec.feature "Admin dashboard link", type: :feature do
+  context 'not signed in as admin user' do
+    it 'does not show a admin button' do
+      visit '/wholesalers'
+      expect(page).to have_link('Coffee Club', :href => '/coffee_club')
+      expect(page).not_to have_link('Admin', :href => '/admin')
+    end
+  end
+  context 'signed in as admin user' do
+    it 'shows an admin button' do
+      admin = create(:admin)
+      visit '/employees/sign_in'
+      within("#new_employee") do
+        fill_in 'Email', with: 'employeedoe11@example.com'
+        fill_in 'Password', with: '12345678'
+      end
+      click_button 'Log in'
+      print page.html
+      visit '/wholesalers'
+      expect(page).to have_link('Admin', :href => '/admin')
+    end
+  end
 end
