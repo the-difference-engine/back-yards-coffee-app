27bcfe98ee3ff9a42c62f394ca587f4d2a10311e
Timothy Meixell <misternu@gmail.com>
2018-04-12 19:52:23 -0500

    Add columns to carted subscriptions table

---
+++ db/migrate/20180413004445_add_columns_to_carted_products.rb
@@ -0,0 +1,10 @@
+class AddColumnsToCartedProducts < ActiveRecord::Migration[5.0]
+  def change
+    add_column  :carted_subscriptions, :products, :jsonb, null: false, default: '{}'
+    add_column  :carted_subscriptions, :plan, :string
+    add_column  :carted_subscriptions, :expired_at, :datetime
+    add_column  :carted_subscriptions, :order_created_at, :datetime
+    add_column  :carted_subscriptions, :next_order_date, :date
+    add_index   :carted_subscriptions, :products, using: :gin
+  end
+end
