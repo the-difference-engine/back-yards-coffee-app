60f6eb2822fe58db4379c4f186ef74eab95c8312
Jay Lockridge <jklockri@pnw.edu>
2018-06-07 20:08:55 -0500

    heroku stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,6 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'qa-back-yards-coffee.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
